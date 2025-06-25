9f31ed72dcc94e30dfa769f8d5c80a0c6fc836da
SirPerzifal <ar351409@Gmail.com>
2025-06-25 08:26:47 +0700

    test

--- src/app/service/api.service.ts
+++ src/app/service/api.service.ts
@@ -7,12 +7,12 @@ import { Injectable } from '@angular/core';
 export class ApiService {
   // readonly baseUrl = 'http://localhost:8069'
   readonly baseUrl = 'https://backend-ifs360.sgeede.com';
-  // protected readonly baseUrl = 'http://192.168.1.144:8069';
+  // readonly baseUrl = 'http://192.168.1.145:8069';
   // readonly baseUrl = 'http://0.0.0.0:8017';
   // protected readonly baseUrl = 'http://127.17.0.1:8073';
   // readonly baseUrl = 'http://192.168.1.161:8017';
   // readonly baseUrl = 'http://192.168.54.230:8017';
-  // readonly baseUrl = 'http://192.168.1.185:8069'
+  // readonly baseUrl = 'http://192.168.1.123:8069'
   // readonly urlItercom = 'http://172.20.10.3:8069';
 
   constructor(protected http: HttpClient) {}
