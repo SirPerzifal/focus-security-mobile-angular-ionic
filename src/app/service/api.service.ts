87a46b3fc2fdc8922ffc2d1d29e827af68133289
SriPerzifal <ar351409@gmail.com>
2025-11-13 11:19:25 +0700

    fixing api

--- src/app/service/api.service.ts
+++ src/app/service/api.service.ts
@@ -5,9 +5,9 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class ApiService {
-  // readonly baseUrl = 'https://ifs360-sg.com';
+  readonly baseUrl = 'https://ifs360-sg.com';
   // readonly baseUrl = 'https://backend-ifs360.sgeede.com';
-  readonly baseUrl = 'http://192.168.1.84:8017';
+  // readonly baseUrl = 'http://192.168.1.84:8017';
   // readonly baseUrl = 'http://localhost:8069';
 
   constructor(protected http: HttpClient) {}
