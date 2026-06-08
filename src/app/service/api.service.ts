fca325715f8c867f79c0a8a2056f063d3afde259
SirPerzifal <ar351409@Gmail.com>
2026-06-08 08:32:54 +0700

    api service live

--- src/app/service/api.service.ts
+++ src/app/service/api.service.ts
@@ -5,11 +5,11 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class ApiService {
-  // readonly baseUrl = 'https://ifs360-sg.com';
+  readonly baseUrl = 'https://ifs360-sg.com';
   // readonly baseUrl = 'https://backend-ifs360.sgeede.com';
   // readonly baseUrl = 'http://192.168.1.212:8069';
   // readonly baseUrl = 'http://192.168.1.217:8017';
-  readonly baseUrl = 'http://10.56.232.230:8017';
+  // readonly baseUrl = 'http://10.56.232.230:8017';
   // readonly baseUrl = 'https://5b8a-2001-448a-8020-17d8-d5c1-973d-a840-c701.ngrok-free.app';
 
   constructor(protected http: HttpClient) { }
